fcf779e39e91af9d040b385a9d0a0dc88aef13ce
Matej <matej.kulhan38@gmail.com>
2023-08-19 11:52:44 +0200

    added variable scrambles

--- scramble.py
+++ scramble.py
@@ -1,17 +1,15 @@
 import random
 
 
-MOVES = ['U', 'D', 'R', 'L', 'F', 'B']
-DIRECTIONS = ['', '\'', '2']
+MOVES: [str] = ['U', 'D', 'R', 'L', 'F', 'B']
+DIRECTIONS: [str] = ['', '\'', '2']
 OPPOSITES = {'U': 'UD', 'D': 'UD', 'R': 'RL', 'L': 'RL', 'F': 'FB', 'B': 'FB'}
 
-SCRAMBLE_LENGTH = 20
 
-
-def generate_scramble() -> str:
+def generate_scramble(scramble_length: int = 20) -> str:
     scramble = [random.choice(MOVES) + random.choice(DIRECTIONS)]
 
-    for i in range(SCRAMBLE_LENGTH - 1):
+    for i in range(scramble_length - 1):
         move = random.choice(MOVES)
         while move in OPPOSITES[scramble[i][0]]:
             move = random.choice(MOVES)
@@ -21,4 +19,9 @@ def generate_scramble() -> str:
     return ' '.join(scramble)
 
 
-print(generate_scramble())
+def scramble_3x3() -> str:
+    return generate_scramble(random.randint(20, 22))
+
+
+def scramble_2x2():
+    return generate_scramble(random.randint(20, 22))
