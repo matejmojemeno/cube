34bd4e20253bca67169b347dc0e0d2312a6daff3
Matej <matej.kulhan38@gmail.com>
2023-09-01 00:43:29 +0200

    fixed assignment

--- scramble_visual.py
+++ scramble_visual.py
@@ -8,45 +8,40 @@ class ScrambleVisual:
     RED: int = 3
     BLUE: int = 4
     YELLOW: int = 5
-    
-    
+
     def __init__(self):
         self.side_colors: list[np.ndarray] = [np.ones((3, 3)) * i for i in range(6)]
 
-
     def right_turn(self):
-        np.rot90(self.side_colors[self.RED], axes=(1,0))
+        self.side_colors[self.RED] = np.rot90(self.side_colors[self.RED], axes=(1, 0))
         green_side_copy = self.side_colors[self.GREEN][:, 2].copy()
         self.side_colors[self.GREEN][:, 2] = self.side_colors[self.YELLOW][:, 2]
         self.side_colors[self.YELLOW][:, 2] = self.side_colors[self.BLUE][:, 0]
         self.side_colors[self.BLUE][:, 0] = self.side_colors[self.WHITE][:, 2]
         self.side_colors[self.WHITE][:, 2] = green_side_copy
 
-
-
     def left_turn(self):
-        np.rot90(self.side_colors[self.ORANGE])
+        self.side_colors[self.ORANGE] = np.rot90(self.side_colors[self.ORANGE])
         green_side_copy = self.side_colors[self.GREEN][:, 0].copy()
         self.side_colors[self.GREEN][:, 0] = self.side_colors[self.WHITE][:, 0]
         self.side_colors[self.WHITE][:, 0] = self.side_colors[self.BLUE][:, 2]
         self.side_colors[self.BLUE][:, 2] = self.side_colors[self.YELLOW][:, 0]
         self.side_colors[self.YELLOW][:, 0] = green_side_copy
 
-
     def up_turn(self):
-        np.rot90(self.side_colors[self.WHITE], axes=(1,0))
-        green_side_copy = self.side_colors[self.GREEN][0,:].copy()
-        self.side_colors[self.GREEN][0,:] = self.side_colors[self.RED][0,:]
-        self.side_colors[self.RED][0,:] = self.side_colors[self.BLUE][0,:]
-        self.side_colors[self.BLUE][0,:] = self.side_colors[self.ORANGE][0,:]
-        self.side_colors[self.ORANGE][0,:] = green_side_copy
-
+        self.side_colors[self.WHITE] = np.rot90(
+            self.side_colors[self.WHITE], axes=(1, 0)
+        )
+        green_side_copy = self.side_colors[self.GREEN][0, :].copy()
+        self.side_colors[self.GREEN][0, :] = self.side_colors[self.RED][0, :]
+        self.side_colors[self.RED][0, :] = self.side_colors[self.BLUE][0, :]
+        self.side_colors[self.BLUE][0, :] = self.side_colors[self.ORANGE][0, :]
+        self.side_colors[self.ORANGE][0, :] = green_side_copy
 
     def down_turn(self):
-        np.rot90(self.side_colors[self.YELLOW])
-        green_side_copy = self.side_colors[self.GREEN][2,:].copy()
-        self.side_colors[self.GREEN][2,:] = self.side_colors[self.RED][2,:]
-        self.side_colors[self.RED][2,:] = self.side_colors[self.BLUE][2,:]
-        self.side_colors[self.BLUE][2,:] = self.side_colors[self.ORANGE][2,:]
-        self.side_colors[self.ORANGE][2,:] = green_side_copy
-        
+        self.side_colors[self.YELLOW] = np.rot90(self.side_colors[self.YELLOW])
+        green_side_copy = self.side_colors[self.GREEN][2, :].copy()
+        self.side_colors[self.GREEN][2, :] = self.side_colors[self.RED][2, :]
+        self.side_colors[self.RED][2, :] = self.side_colors[self.BLUE][2, :]
+        self.side_colors[self.BLUE][2, :] = self.side_colors[self.ORANGE][2, :]
+        self.side_colors[self.ORANGE][2, :] = green_side_copy
